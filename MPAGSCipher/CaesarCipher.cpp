331b0f31a8567bebb71cc77708510cf29f3caf4a
Thomas Latham <T.Latham@warwick.ac.uk>
2018-11-07 19:12:04 +0000

    TEL review of day 3 homework

--- MPAGSCipher/CaesarCipher.cpp
+++ MPAGSCipher/CaesarCipher.cpp
@@ -2,28 +2,32 @@
 #include "CipherMode.hpp"
 #include <iostream>
 #include <string>
-CaesarCipher::CaesarCipher(const size_t& key):key_{key}
+
+CaesarCipher::CaesarCipher(const size_t& key):key_{key % alphabetSize_}
 {
-}
-CaesarCipher::CaesarCipher(const std::string& key):key_{0}
-{
-  for ( const auto& elem : key ) {
-      if ( ! std::isdigit(elem) ) {
-        std::cerr << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
-                  << "        the supplied key (" << key << ") could not be successfully converted" << std::endl;
-	break;
-      }
-      else {
-	key_ = std::stoul(key);
-      }
-  }
 }
 
-std::string CaesarCipher::applyCipher(const std::string& inputText, const CipherMode mode )
+CaesarCipher::CaesarCipher(const std::string& key):key_{0}
 {
-  // Make sure that the key is in the range 0 - 25
-  const size_t alphabetSize = alphabet_.size();
-  const size_t truncatedKey { key_ % alphabetSize };
+  if ( key.empty() ) {
+    return;
+  }
+
+  for ( const auto& elem : key ) {
+    if ( ! std::isdigit(elem) ) {
+      std::cerr << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
+	        << "        the supplied key (" << key << ") could not be successfully converted" << std::endl;
+      return;
+    }
+  }
+
+  key_ = std::stoul(key) % alphabetSize_;
+}
+
+std::string CaesarCipher::applyCipher(const std::string& inputText, const CipherMode mode ) const
+{
+  // Create the output string
+  std::string outputText {};
 
   // Loop over the input text
   char processedChar {'x'};
@@ -31,26 +35,26 @@ std::string CaesarCipher::applyCipher(const std::string& inputText, const Cipher
 
     // For each character in the input text, find the corresponding position in
     // the alphabet by using an indexed loop over the alphabet container
-    for ( size_t i{0}; i < alphabetSize; ++i ) {
+    for ( size_t i{0}; i < alphabetSize_; ++i ) {
 
       if ( origChar == alphabet_[i] ) {
 
         // Apply the appropriate shift (depending on whether we're encrypting
         // or decrypting) and determine the new character
         // Can then break out of the loop over the alphabet
-        if ( mode == CipherMode::encrypt ) {
-          processedChar = alphabet_[ (i + truncatedKey) % alphabetSize ];
+        if ( mode == CipherMode::Encrypt ) {
+          processedChar = alphabet_[ (i + key_) % alphabetSize_ ];
         } 
-	else if (mode == CipherMode::decrypt) {
-	  processedChar = alphabet_[ (i + alphabetSize - truncatedKey) % alphabetSize ];
+	else if (mode == CipherMode::Decrypt) {
+	  processedChar = alphabet_[ (i + alphabetSize_ - key_) % alphabetSize_ ];
         }
         break;
       }
     }
 
     // Add the new character to the output text
-    outputText_ += processedChar;
+    outputText += processedChar;
   }
 
-  return outputText_;
+  return outputText;
 }
--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 // Our project headers
+#include "CipherMode.hpp"
 #include "TransformChar.hpp"
 #include "ProcessCommandLine.hpp"
 #include "CaesarCipher.hpp"
@@ -16,7 +17,7 @@ int main(int argc, char* argv[])
   const std::vector<std::string> cmdLineArgs {argv, argv+argc};
 
   // Options that might be set by the command-line arguments
-  ProgramSettings commands {false,false,"","","",CipherMode::encrypt};
+  ProgramSettings commands {false,false,"","","",CipherMode::Encrypt};
 
   // Process command line arguments
   bool cmdLineStatus { processCommandLine(cmdLineArgs, commands) };
@@ -87,26 +88,11 @@ int main(int argc, char* argv[])
     }
   }
 
-  // We have the key as a string, but the Caesar cipher needs an unsigned long, so we first need to convert it
-  // We default to having a key of 0, i.e. no encryption, if no key was provided on the command line
-
-    // Before doing the conversion we should check that the string contains a
-    // valid positive integer.
-    // Here we do that by looping through each character and checking that it
-    // is a digit. What is rather hard to check is whether the number is too
-    // large to be represented by an unsigned long, so we've omitted that for
-    // the time being.
-    // (Since the conversion function std::stoul will throw an exception if the
-    // string does not represent a valid unsigned long, we could check for and
-    // handled that instead but we only cover exceptions very briefly on the
-    // final day of this course - they are a very complex area of C++ that
-    // could take an entire course on their own!)
-    CaesarCipher cipherkey{commands.cipher_key};
+  // Create a Caesar cipher (using the specified key)
+  CaesarCipher cipherkey{commands.cipher_key};
 
   // Run the Caesar cipher from the applyCipher member function on the input text
-  CipherMode mode {CipherMode::encrypt};
-  mode = commands.mode; 
-  std::string outputText {cipherkey.applyCipher(inputText, mode)};
+  std::string outputText {cipherkey.applyCipher(inputText, commands.mode)};
 
   // Output the transliterated text
   if (!commands.outputFile.empty()) {
