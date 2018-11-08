a2f7b6c567e347f0ed009f94af908e0ee8f13e13
Thomas Latham <T.Latham@warwick.ac.uk>
2018-11-08 16:05:40 +0000

    TEL review of unit tests

--- Testing/testProcessCommandLine.cpp
+++ Testing/testProcessCommandLine.cpp
@@ -18,7 +18,6 @@ TEST_CASE("Reads command line arguments correctly","[command line]"){
   SECTION("Process --help correctly"){
     const std::vector<std::string> testargs {"name", "--help"};
     REQUIRE (processCommandLine(testargs, testcommands)==true);
-    
     REQUIRE (testcommands.helpRequested==true);
   }
 
@@ -28,36 +27,53 @@ TEST_CASE("Reads command line arguments correctly","[command line]"){
     REQUIRE (testcommands.versionRequested==true);
   }
 
-    SECTION("Process -i correctly"){
-    const std::vector<std::string> testargs {"name", "-i","input"};
-    REQUIRE (processCommandLine(testargs, testcommands)==true);
-    REQUIRE (testcommands.inputFile=="input");
+  SECTION("Process -i correctly"){
+    SECTION("With no filename specified"){
+      const std::vector<std::string> testargs {"name", "-i"};
+      REQUIRE (processCommandLine(testargs, testcommands)==false);
+    }
+    SECTION("With filename specified"){
+      const std::vector<std::string> testargs {"name", "-i","input"};
+      REQUIRE (processCommandLine(testargs, testcommands)==true);
+      REQUIRE (testcommands.inputFile=="input");
+    }
   }
 
   SECTION("Process -o correctly"){
-    const std::vector<std::string> testargs {"name", "-o","output"};
-    REQUIRE (processCommandLine(testargs, testcommands)==true);
-    REQUIRE (testcommands.outputFile=="output");
+    SECTION("With no filename specified"){
+      const std::vector<std::string> testargs {"name", "-o"};
+      REQUIRE (processCommandLine(testargs, testcommands)==false);
+    }
+    SECTION("With filename specified"){
+      const std::vector<std::string> testargs {"name", "-o","output"};
+      REQUIRE (processCommandLine(testargs, testcommands)==true);
+      REQUIRE (testcommands.outputFile=="output");
+    }
   }
 
   SECTION("Process -k correctly"){
-    const std::vector<std::string> testargs {"name", "-k","5"};
-    REQUIRE (processCommandLine(testargs, testcommands)==true);
-    REQUIRE (testcommands.cipher_key=="5");
-  }
-
-  SECTION("Process help correctly"){
-    const std::vector<std::string> testargs {"name", "-h"};
-    REQUIRE (processCommandLine(testargs, testcommands)==true);
-    REQUIRE (testcommands.helpRequested==true);
+    SECTION("With value not specified"){
+      const std::vector<std::string> testargs {"name", "-k"};
+      REQUIRE (processCommandLine(testargs, testcommands)==false);
+    }
+    SECTION("With value specified"){
+      const std::vector<std::string> testargs {"name", "-k","5"};
+      REQUIRE (processCommandLine(testargs, testcommands)==true);
+      REQUIRE (testcommands.cipher_key=="5");
+    }
   }
 
   SECTION("Process mode correctly"){
-    const std::vector<std::string> testargs {"name", "--encrypt"};
-    REQUIRE (processCommandLine(testargs, testcommands)==true);
-    REQUIRE (testcommands.mode==CipherMode::Encrypt);
+    SECTION("For encryption"){
+      const std::vector<std::string> testargs {"name", "--encrypt"};
+      REQUIRE (processCommandLine(testargs, testcommands)==true);
+      REQUIRE (testcommands.mode==CipherMode::Encrypt);
+    }
+    SECTION("For decryption"){
+      const std::vector<std::string> testargs {"name", "--decrypt"};
+      REQUIRE (processCommandLine(testargs, testcommands)==true);
+      REQUIRE (testcommands.mode==CipherMode::Decrypt);
+    }
   }
 
 }
-
-  
